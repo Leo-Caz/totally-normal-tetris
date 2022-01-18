43013786348f013679ee610020660b0a01a08317
Léo Cazenave <leo@cazenave.cc>
2022-01-18 16:40:48 +0100

    Spawn a new block when the previous one fell.

--- Assets/Scripts/bloc_script.cs
+++ Assets/Scripts/bloc_script.cs
@@ -13,16 +13,13 @@ public class bloc_script : MonoBehaviour
 		if (!hasLanded) {
 			float horizontalMove = Input.GetAxisRaw("Horizontal");
 			rb.AddForce(new Vector2(horizontalMove * movementSpeed, 0f), ForceMode2D.Force);
-			/* Debug.Log("prout"); */
 		}
-		/* Debug.Log("prout2"); */
 	}
 
 	void OnCollisionEnter2D(Collision2D collision) {
 		foreach (ContactPoint2D contact in collision.contacts) {
 			int contactLayer = contact.collider.gameObject.layer;
 			if (contactLayer == LayerMask.NameToLayer("Blocs") || contactLayer == LayerMask.NameToLayer("Ground")) {
-				Debug.Log("uuu");
 				hasLanded = true;
 			}
 		}
--- Assets/Scripts/dropper_script.cs
+++ Assets/Scripts/dropper_script.cs
@@ -6,16 +6,27 @@ public class dropper_script : MonoBehaviour
 {
 	// References to the different falling blocs (prefabs):
 	public GameObject bloc;
+	private GameObject currentBlock;  // Reference to last bloc spawned
+	private bloc_script currentBlockScript;  // Reference to the script of that bloc
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        Instantiate(bloc, transform.position, Quaternion.identity);
+    void createBlock() {
+		// Create an instance of the prefab and keep a reference to this instance
+		currentBlock = Instantiate(bloc, transform.position, Quaternion.identity) as GameObject;
+		if (currentBlock != null) {
+			currentBlockScript = currentBlock.GetComponent<bloc_script>();
+		}
     }
 
-    /* // Update is called once per frame */
-    /* void Update() */
-    /* { */
-        
-    /* } */
+    // Start is called before the first frame update
+	void Awake() {
+		createBlock();
+	}
+
+    // Update is called once per frame
+    void Update() {
+		// If the bloc has landed, create a new one
+		if (currentBlockScript.hasLanded) {
+			createBlock();
+		}
+    }
 }
